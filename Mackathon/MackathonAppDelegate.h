f723d8b9a9c49430c7fd97c1ce2f70667340fce6
Matthew Holden <jftholden@yahoo.com>
2012-01-29 12:49:25 -0800

    Initial Commit

---
+++ Mackathon/MackathonAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MackathonAppDelegate.h
+//  Mackathon
+//
+//  Created by Matthew Holden on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MackathonAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mackathon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mackathon
+//
+//  Created by Matthew Holden on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MackathonAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([MackathonAppDelegate class]));
+	}
+}
